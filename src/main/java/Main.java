c89c4adffc429dbc44664d0e647bbef2de6cd946
Михаил Красноперов <muxajlu-4@yandex.ru>
2020-12-22 00:33:04 +0400

    исправление замечаний: повторяющийся код вынесен в функции, добавлен javadoc на новые функции

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,7 +15,7 @@ public class Main {
    * в файле {@code filePath} формата CSV.
    *
    * @param filePath путь к файлу.
-   * @param user пользователь.
+   * @param user     пользователь.
    */
   public static void create(@NotNull String filePath, @NotNull User user) {
     Map<Integer, String> usersInfo = new HashMap<>();
@@ -30,18 +30,12 @@ public class Main {
         }
       }
       usersInfo.put(id,
-                user.secondName + "," + user.firstName + ","
-                + (user.middleName != null ? user.middleName : "\"\"") + "," + user.age);
+              user.secondName + "," + user.firstName + ","
+                      + (user.middleName != null ? user.middleName : "\"\"") + "," + user.age);
     } catch (IOException e) {
       e.printStackTrace();
     }
-    try (BufferedWriter wr = new BufferedWriter(new FileWriter(filePath))) {
-      for (Map.Entry<Integer, String> item : usersInfo.entrySet()) {
-        wr.write(item.getKey() + "," + item.getValue() + "\n");
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    writer(filePath, id, usersInfo);
   }
 
   /**
@@ -50,7 +44,7 @@ public class Main {
    * с таким идентификатором не существует.
    *
    * @param filePath путь к файлу.
-   * @param id идентификатор записи о пользователе.
+   * @param id       идентификатор записи о пользователе.
    * @return Пользователь или {@code null}.
    */
   public static @Nullable User read(@NotNull String filePath, int id) {
@@ -60,9 +54,9 @@ public class Main {
         String[] usersArr = users.split(",");
         if (id == Integer.parseInt(usersArr[0])) {
           return new User(Integer.parseInt(usersArr[4]),
-               usersArr[1],
-               usersArr[2],
-               usersArr[3].equals("\"\"") ? null : usersArr[3]);
+                  usersArr[1],
+                  usersArr[2],
+                  usersArr[3].equals("\"\"") ? null : usersArr[3]);
         }
       }
       return null;
@@ -78,35 +72,18 @@ public class Main {
    * на информацию пользователя {@code user}.
    *
    * @param filePath путь к файлу.
-   * @param id идентификатор записи о пользователе.
-   * @param user пользователь.
+   * @param id       идентификатор записи о пользователе.
+   * @param user     пользователь.
    * @throws java.util.NoSuchElementException запись о пользователе
-   *     с идентификатором {@code id} не существует.
+   *                                          с идентификатором {@code id} не существует.
    */
   public static void update(@NotNull String filePath, int id, @NotNull User user) {
-    Map<Integer, String> usersInfo = new HashMap<>();
-    try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-      String users;
-      while ((users = br.readLine()) != null) {
-        String[] userArr = users.split(",", 2);
-        usersInfo.put(Integer.parseInt(userArr[0]), userArr[1]);
-      }
-      if (!usersInfo.containsKey(id)) {
-        throw new NoSuchElementException();
-      }
-      usersInfo.put(id,
+    Map<Integer, String> usersInfo;
+    usersInfo = reader(filePath, id);
+    usersInfo.put(id,
               user.secondName + "," + user.firstName + ","
-                    + (user.middleName != null ? user.middleName : "\"\"") + "," + user.age);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    try (BufferedWriter wr = new BufferedWriter(new FileWriter(filePath))) {
-      for (Map.Entry<Integer, String> item : usersInfo.entrySet()) {
-        wr.write(item.getKey() + "," + item.getValue() + "\n");
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+                      + (user.middleName != null ? user.middleName : "\"\"") + "," + user.age);
+    writer(filePath, id, usersInfo);
   }
 
   /**
@@ -114,12 +91,28 @@ public class Main {
    * о пользователе с идентификатором {@code id}.
    *
    * @param filePath путь к файлу.
-   * @param id идентификатор записи о пользователе.
+   * @param id       идентификатор записи о пользователе.
    * @throws java.util.NoSuchElementException запись о пользователе
-   *     с идентификатором {@code id} не существует.
+   *                                          с идентификатором {@code id} не существует.
    */
-
   public static void delete(@NotNull String filePath, int id) {
+    Map<Integer, String> usersInfo;
+    usersInfo = reader(filePath, id);
+    usersInfo.remove(id);
+    writer(filePath, id, usersInfo);
+  }
+
+  /**
+   * Создает коллекцию пользователей из файла {@code filePath} формата CSV
+   * идентификатором {@code id}. Возвращает коллекцию {@code Map<Integer, String>} данных
+   * о существующих пользователях.
+   *
+   * @param filePath путь к файлу.
+   * @param id       идентификатор записи о пользователе.
+   * @throws java.util.NoSuchElementException запись о пользователе
+   *                                          с идентификатором {@code id} не существует.
+   */
+  public static Map<Integer, String> reader(String filePath, int id) {
     Map<Integer, String> usersInfo = new HashMap<>();
     try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
       String users;
@@ -133,7 +126,18 @@ public class Main {
     } catch (IOException e) {
       e.printStackTrace();
     }
-    usersInfo.remove(id);
+    return usersInfo;
+  }
+
+  /**
+   * Записывает в файл {@code filePath} формата CSV
+   * нового пользователя с идентификатором {@code id}.
+   *
+   * @param filePath путь к файлу.
+   * @param id       идентификатор записи о пользователе.
+   * @param usersInfo       идентификатор записи о пользователе.
+   */
+  public static void writer(String filePath, int id, Map<Integer, String> usersInfo) {
     try (BufferedWriter wr = new BufferedWriter(new FileWriter(filePath))) {
       for (Map.Entry<Integer, String> item : usersInfo.entrySet()) {
         wr.write(item.getKey() + "," + item.getValue() + "\n");
